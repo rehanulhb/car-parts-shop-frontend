6fe4f07500c696ac487e95ba46497790f1182151
rehanulhb <rehanulh@gmail.com>
2025-01-12 03:02:34 +0100

    SignUp Page Created, Auth Context and Routes Created

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,29 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import img from '../../assets/images/login/login.svg'
 
-const Login = () => {
-    const handleLogin = event =>{
+const SignUp = () => {
+    const handleSignUp = event =>{
         event.preventDefault();
     }
-
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
         <div className="w-1/2 mr-12">
-          
           <img src={img} alt="" srcset="" />
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form onSubmit={handleLogin} className="card-body">
-          <h1 className="text-3xl font-bold text-center">Login </h1>
+          <form onSubmit={handleSignUp} className="card-body">
+            <h1 className="text-3xl font-bold text-center">Sign Up </h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
-                name='email'
-                placeholder="email"
+                name="email"
+                placeholder="Email"
                 className="input input-bordered"
                 required
               />
@@ -34,8 +44,8 @@ const Login = () => {
               </label>
               <input
                 type="password"
-                name='password'
-                placeholder="password"
+                name="password"
+                placeholder="Password"
                 className="input input-bordered"
                 required
               />
@@ -46,15 +56,19 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              
               <input className="btn btn-primary" type="submit" value="Login" />
             </div>
           </form>
-          <p className='my-4 text-center'>New to car Shop <Link className='text-orange-600 font-bold' to="/signup">Sign up</Link></p>
+          <p className="my-4 text-center">
+            Already have an account? {""}
+            <Link className="text-orange-600 font-bold" to="/login">
+               Login
+            </Link>
+          </p>
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default SignUp;
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import AuthProvider from './Providers/AuthProvider.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
     <StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </StrictMode>,
   </div>
 ) 
