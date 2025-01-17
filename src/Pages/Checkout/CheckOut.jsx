58ab10120cd5b56d953af1c008d61374a1e05179
rehanulhb <rehanulh@gmail.com>
2025-01-17 02:00:52 +0100

    Working on Checkout Frontend Page to save Data to MongoDB

--- src/Pages/Checkout/CheckOut.jsx
+++ src/Pages/Checkout/CheckOut.jsx
@@ -1,54 +1,95 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { title, _id } = service;
+  const { title, _id, price, img } = service;
+  const {user} = useContext(AuthContext);
+
+  const handleBookService = event =>{
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const booking = {
+      customerName : name,
+      email, 
+      img,
+      date,
+      service: title,
+      service_id: _id,
+      price: price
+    }
+    console.log(booking);
+
+    fetch('http://localhost:5000/bookings', {
+      method: 'POST',
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify(booking)
+
+    })
+    .then(res => res.json())
+    .then(data=>{
+      console.log(data)
+    })
+
+  }
+
   return (
     <div>
-      <h2>Checkout: {title}</h2>
+      <h2 className="text-center text-3xl">Checkout: {title}</h2>
 
-      <form className="card-body">
+      <form onSubmit={handleBookService} className="card-body">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="form-control">
             <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              defaultValue={user?.displayName}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <input
+              type="date"
+              name="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
               type="email"
-              placeholder="email"
+              name="email"
+              placeholder="Email"
+              defaultValue={user?.email}
               className="input input-bordered"
               required
             />
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text">Due Amount</span>
             </label>
             <input
-              type="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
+              type="text"
+              defaultValue={'$'+ price}
               className="input input-bordered"
               required
             />
