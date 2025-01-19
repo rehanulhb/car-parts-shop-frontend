5a048f2b215c11f2d88ff21077b6b47c1eb3efdb
rehanulhb <rehanulh@gmail.com>
2025-01-19 04:21:18 +0100

    Update Function done using Patch

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,5 +1,5 @@
-const BookingRow = ({ booking, handleDelete }) => {
-  const { _id, date, service, price, img } = booking;
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
+  const { _id, date, service, price, img, status } = booking;
 
   
 
@@ -34,7 +34,9 @@ const BookingRow = ({ booking, handleDelete }) => {
       <td>{date}</td>
       <td>${price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        { 
+          status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> : 
+          <button onClick={()=> handleBookingConfirm(_id)} className="btn btn-ghost btn-xs">Please Confirm</button>}
       </th>
     </tr>
   );
